df721b7b099087f5a620cecc46e3c206d86ee786
Marta Adamska <57840378+whtvrkaa@users.noreply.github.com>
2022-07-06 16:06:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+const express = require('express');
+const app = express();
+const quotesJSONArray = require('./quotes.json');
+const port = 3001;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/quotesList',(req, res) => {
+    res.send({ 'quotesList': quotesJSONArray });
+});
+
+app.get('/randomQuote',(req, res)=>{
+    const randomNumber = Math.floor(Math.random() * 21);
+    const quoteItem = quotesJSONArray[randomNumber]; 
+    res.send(quoteItem);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
